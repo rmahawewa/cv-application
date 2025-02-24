9d47a1b17d9e31b1212b4618fffe764ad3025e6c
rmahawewa <rmahawewa@yahoo.com>
2025-02-24 18:39:55 +0530

    create and list eduinfo object array elements

--- src/EducationalQualifications.jsx
+++ src/EducationalQualifications.jsx
@@ -7,27 +7,34 @@ export default function EducationalQualifications() {
     const [titleOfStudy, setTitleOfStudy] = useState('');
     const [dateOfStudy, setDateOfStudy] = useState('');
     const [eduInfo, setEduInfo] = useState([]);
-    const [isEditing, setIsEditing] = useState([]);
+    
 
     function handleEduQualAddButtonClick(){
         setEduInfo([
             ...eduInfo,
             { id: nextId++, schoolName: schoolName, titleOfStudy: titleOfStudy, dateOfStudy: dateOfStudy }
         ]);
-
-        setIsEditing([
-            ...isEditing,
-            { id: nextId++, schoolNameEditing: false, titleOfStudyEditing: false, dateOfStudyEditing: false }
-        ]);
+        
     }
-
-    function handleIsSchoolNameEditingButtonClick(editId, status){
-        const editingList = [...isEditing];
-        const entity = editingList.find(
-            a => a.id === editId
-        );
-        entity.schoolNameEditing = status;
-        setIsEditing(editingList);
+//react dev tools -> browsers dev tools -> components -> educationInfo component 
+    function handleEducationalQualificationOnchange(id, value, event){
+        console.log(eduInfo[0].schoolName);
+        const eduInformation = [...eduInfo];
+        const row = eduInformation.find(
+            a => a.id === id
+        );     
+        if (row){
+            if (event && event.target){
+                row[value] = event.target.value;
+                console.log(row);
+                setEduInfo(eduInformation);
+            }else {
+                console.error("Event or event.target is undefined.");
+            }            
+        }else{
+            console.error(`Row with id ${id} not found.`);
+        }   
+        
     }
 
     return (
@@ -55,36 +62,87 @@ export default function EducationalQualifications() {
                 Add
             </button>
 
+            <EduInfoList 
+                eduInfo = {eduInfo}
+            />
+
+        </>
+    );
+}
+
+function EduInfoList(
+    {eduInfo}
+){
+    const [isEditing, setIsEditing] = useState(false);
+
+    return(
+        <>
             <ul>
+            {isEditing ? (
+            <>             
+                {eduInfo.map((info, index) => (
+                    
+                <li key={info.id}>
+                    <ul>
+                    <li>
+                        <label>School name: 
+                            <input 
+                                value = {info.schoolName}
+                                // onChange = {(e) => handleEducationalQualificationOnchange(info.id, "schoolName", e)}
+                            />
+                        </label>
+                    </li>
+                    <li>
+                        <label>Title of study:
+                            <input 
+                                value = {info.titleOfStudy}
+                                // onChange = {(e) => handleEducationalQualificationOnchange(info.id, "titleOfStudy", e)}
+                            />
+                        </label>
+                    </li>
+                    <li>
+                        <label>Date of study:
+                            <input 
+                                value = {info.dateOfStudy}
+                                // onChange = {(e) => handleEducationalQualificationOnchange(info.id, "dateOfStudy", e)}
+                            />
+                        </label>
+                    </li>
+                    </ul>
+                </li>
+                ))}                 
+                {eduInfo.length > 0 && ( 
+                    <button onClick={() => setIsEditing(false)}>
+                    Submit</button>
+                )}                
+            </>
+            ):(
+                //Non editing content              
+            <>
                 {eduInfo.map((info, index) => (
                     <li key={info.id}>
-                        <ul> 
-                            {isEditing[index].schoolNameEditing ? (
-                                <li><label>School name: 
-                                    <input 
-                                        value={info.schoolName}
-
-                                    />
-                                </label>
-                                    <button onClick={handleIsSchoolNameEditingButtonClick(index, false)}>
-                                        Submit
-                                    </button>
-                                </li>
-                            ):(
-                                <li><label>School name: 
-                                {info.schoolName}
-                                </label>
-                                <button onClick={handleIsSchoolNameEditingButtonClick(index, true)}>Edit</button>
-                                </li>
-                            )}                           
-                            <li>School name: <label>{info.schoolName}</label></li>
-                            <li>Title of study: <label>{info.titleOfStudy}</label></li>
-                            <li>Date of study: <label>{info.dateOfStudy}</label></li>
+                        <ul>
+                            <li>
+                                <label>School name: {info.schoolName}</label>
+                            </li>
+                            <li>
+                                <label>Title of study: {info.titleOfStudy}</label>
+                            </li>
+                            <li>
+                                <label>Date of study: {info.dateOfStudy}</label>
+                            </li>
                         </ul>
                     </li>
                 ))}
+                {eduInfo.length > 0 && ( //not working
+                    <button onClick={() => setIsEditing(true)}>
+                    Edit</button>
+                )}
+                    
+            </>
+            )}
+                    
             </ul>
-
         </>
     );
 }
